7baeb2e213a430c68cba3fcb4c6382101bf8d14d
Dr.Rabies <mikhail.shirokov.2007@mail.ru>
2026-03-07 16:47:48 +0300

    Добавление ORM модели филиала

--- src/database/orm_models.py
+++ src/database/orm_models.py
@@ -1,3 +1,11 @@
 from src.database.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
 
 
+class BranchesORM(Base):
+    __tablename__ = "branches"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    branches_name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    branches_address: Mapped[str] = mapped_column(nullable=False, unique=True)
+    branches_phone: Mapped[str] = mapped_column(nullable=False, unique=True)
+    is_active_for_order: Mapped[bool] = mapped_column(default=True, nullable=False)

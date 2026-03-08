6202d79a5377724566923fe133304f03d2dc0995
Dr.Rabies <mikhail.shirokov.2007@mail.ru>
2026-03-08 22:41:32 +0300

    Завершение шаблона главной страницы каталога сайта

--- src/site_api/routes/html_routes.py
+++ src/site_api/routes/html_routes.py
@@ -2,13 +2,19 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from src.config import settings
 
+from src.site_api.views import get_categories_info, get_branches_info
+
 html_route = APIRouter(prefix="/site")
 templates = Jinja2Templates(directory=settings.DIRECTORY_NAME + "/templates")
 
 
 @html_route.get("/")
 def index(request: Request):
-    pass
+    branches = get_branches_info()
+    categories = get_categories_info()
+
     return templates.TemplateResponse(
-         request=request, name="base_template.html"
+        request=request,
+        name="index.html",
+        context={"branches": branches, "categories": categories}
     )
--- src/site_api/routes/site_data_routes.py
+++ src/site_api/routes/site_data_routes.py
@@ -4,29 +4,3 @@ from src.site_api.views import *
 from src.site_api.dto_models import *
 
 site_data_route = APIRouter(prefix="/sitedata")
-
-
-@site_data_route.get(
-    path="/branches",
-    tags=["Запросы для контента сайта📂"],
-    name="Получить информацию о филиалах",
-    summary="При помощи данного запроса осуществляется передача контактной информации о филиалах организации "
-            "(id, адрес, телефон) необходимых для отображения в подвале сайта и на форме для оформления заказа.",
-    response_class=JSONResponse
-)
-def get_branches_data_for_site():
-    branches = get_branches_info()
-    return JSONResponse(content={"branches": branches}, status_code=status.HTTP_200_OK)
-
-
-@site_data_route.get(
-    path="/categories",
-    tags=["Запросы для контента сайта📂"],
-    name="Получить информацию о категориях",
-    summary="При помощи данного запроса осуществляется передача необходимой для отображения на сайте информации о "
-            "категориях товаров (id, имя категории, порядок отображения).",
-    response_class=JSONResponse
-)
-def get_category_data_for_site():
-    categories = get_categories_info()
-    return JSONResponse(content={"categories": categories}, status_code=status.HTTP_200_OK)

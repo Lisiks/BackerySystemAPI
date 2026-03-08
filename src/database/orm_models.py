ffaf128790c9f8eb3630f85ec321cb5b2bdde018
Dr.Rabies <mikhail.shirokov.2007@mail.ru>
2026-03-08 16:02:46 +0300

    Добавление ORM модели категорий изделий

--- src/database/orm_models.py
+++ src/database/orm_models.py
@@ -8,4 +8,14 @@ class BranchesORM(Base):
     branches_name: Mapped[str] = mapped_column(nullable=False, unique=True)
     branches_address: Mapped[str] = mapped_column(nullable=False, unique=True)
     branches_phone: Mapped[str] = mapped_column(nullable=False, unique=True)
-    is_active_for_order: Mapped[bool] = mapped_column(default=True, nullable=False)
+    is_active_for_order: Mapped[bool] = mapped_column(nullable=False)
+
+
+class CategoriesORM(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category_name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    category_description: Mapped[str] = mapped_column(nullable=True)
+    showing_number: Mapped[int] = mapped_column(unique=True, nullable=False)
+    display_on_site: Mapped[bool] = mapped_column(nullable=False)
+

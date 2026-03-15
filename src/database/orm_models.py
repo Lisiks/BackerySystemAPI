34c479b044f64216397863afd8537db6d0ef8b57
Dr.Rabies <mikhail.shirokov.2007@mail.ru>
2026-03-15 08:48:52 +0300

    Создание relation ship между изделиями и категориями

--- src/database/orm_models.py
+++ src/database/orm_models.py
@@ -1,6 +1,7 @@
 from src.database.database import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
+
 
 class BranchesORM(Base):
     __tablename__ = "branches"
@@ -19,29 +20,32 @@ class CategoriesORM(Base):
     showing_number: Mapped[int] = mapped_column(unique=True, nullable=False)
     display_on_site: Mapped[bool] = mapped_column(nullable=False)
 
+    products: Mapped[list['ProductsORM']] = relationship(
+        back_populates="category"
+    )
+
+
 class ProductsORM(Base):
     __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-
     category_id: Mapped[int] = mapped_column(
         ForeignKey("categories.id"),
         nullable=False
     )
-
     sale_price: Mapped[float] = mapped_column(nullable=False)
     cost_price: Mapped[float] = mapped_column(nullable=False)
-
     composition: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
-
     calories: Mapped[int] = mapped_column(nullable=False)
     protein: Mapped[float] = mapped_column(nullable=False)
     fat: Mapped[float] = mapped_column(nullable=False)
     carbs: Mapped[float] = mapped_column(nullable=False)
-
     weight: Mapped[int] = mapped_column(nullable=False)
     image_url: Mapped[str] = mapped_column(nullable=False)
-
     is_visible: Mapped[bool] = mapped_column(nullable=False, default=True)
+
+    category: Mapped['CategoriesORM'] = relationship(
+        back_populates="products"
+    )

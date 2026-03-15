b22e37e5f35f599fb3646e945253abf1b148b399
Fileger <144237631+Fileger@users.noreply.github.com>
2026-03-15 16:18:03 +0300

    Аутентификация пользователя

--- src/login_api/dto_models.py
+++ src/login_api/dto_models.py
@@ -11,3 +11,12 @@ class RegisterUserResponseDTO(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
+class AuthenticateUserRequestDTO(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=8, max_length=72)
+
+
+class AuthenticateUserResponseDTO(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
--- src/login_api/routes.py
+++ src/login_api/routes.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 
 from src.database.database import session_fabric
-from src.login_api.dto_models import RegisterUserRequestDTO, RegisterUserResponseDTO
-from src.login_api.views import register_user
+from src.login_api.dto_models import (RegisterUserRequestDTO, RegisterUserResponseDTO, AuthenticateUserRequestDTO,
+                                      AuthenticateUserResponseDTO)
+from src.login_api.views import register_user, authenticate_user
 
-login_route = APIRouter(prefix="/login")
+
+login_route = APIRouter(prefix="/login", tags=["Логин"])
 
 def get_db():
     session = session_fabric()
@@ -15,10 +17,26 @@ def get_db():
         session.close()
 
 
-@login_route.post("/register", response_model=RegisterUserResponseDTO, status_code=201)
+@login_route.post("/register",
+                  response_model=RegisterUserResponseDTO,
+                  status_code=201,
+                  summary="Регистрация пользователя")
 def register_user_route(
     data: RegisterUserRequestDTO,
     response: Response,
     session: Session = Depends(get_db),
 ):
     return register_user(data=data, response=response, session=session)
+
+@login_route.post(
+    "/authenticate",
+    response_model=AuthenticateUserResponseDTO,
+    status_code=200,
+    summary="Аутентификация пользователя"
+)
+def authenticate_user_route(
+    data: AuthenticateUserRequestDTO,
+    response: Response,
+    session: Session = Depends(get_db),
+):
+    return authenticate_user(data=data, response=response, session=session)
